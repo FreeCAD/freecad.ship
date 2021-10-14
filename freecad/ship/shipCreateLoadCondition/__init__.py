292a710ca76f34e31ce1ace98d2720683e56d319
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2021-10-14 16:40:34 +0200

    Upgraded the load conditions creation tool

--- freecad/ship/shipCreateLoadCondition/__init__.py
+++ freecad/ship/shipCreateLoadCondition/__init__.py
@@ -24,50 +24,20 @@ import FreeCAD as App
 import FreeCADGui as Gui
 from PySide import QtGui
 from . import Tools
-
-
-READ_ONLY_FOREGROUND = (0.5, 0.5, 0.5)
-READ_ONLY_BACKGROUND = (0.9, 0.9, 0.9)
+from ..shipUtils import Selection
 
 
 def load():
     """Directly create the load condition"""
     # Check that a ship has been selected
-    ship = None
-    selObjs = Gui.Selection.getSelection()
-    if not selObjs:
+    sel_ships = Selection.get_ships()
+    if not sel_ships:
         msg = QtGui.QApplication.translate(
             "ship_console",
-            "A ship instance must be selected before using this tool (no"
-            " objects selected)",
+            "A ship instance must be selected before using this tool",
             None)
         App.Console.PrintError(msg + '\n')
-        return
-    for i in range(len(selObjs)):
-        obj = selObjs[i]
-        props = obj.PropertiesList
-        try:
-            props.index("IsShip")
-        except ValueError:
-            continue
-        if obj.IsShip:
-            if ship:
-                msg = QtGui.QApplication.translate(
-                    "ship_console",
-                    "More than one ship have been selected (the extra"
-                    " ships will be ignored)",
-                    None)
-                App.Console.PrintWarning(msg + '\n')
-                break
-            ship = obj
-
-    if not ship:
-        msg = QtGui.QApplication.translate(
-            "ship_console",
-            "A ship instance must be selected before using this tool (no"
-            " valid ship found at the selected objects)",
-            None)
-        App.Console.PrintError(msg + '\n')
-        return
-
-    Tools.createLoadCondition(ship)
+        return True
+    for ship in sel_ships:
+        lc = Tools.createLoadCondition(ship)
+        lc.ViewObject.doubleClicked()
