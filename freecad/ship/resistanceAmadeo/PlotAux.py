de9b3536576d3bd66c2f1cf45cb11ab42e8df87e
Juan Manuel <juanma.munozgodin@gmail.com>
2023-04-18 19:08:23 +0200

    Plotting graphs resistance coefficients vs speed

--- freecad/ship/resistanceAmadeo/PlotAux.py
+++ freecad/ship/resistanceAmadeo/PlotAux.py
@@ -21,19 +21,22 @@
 #***************************************************************************
 
 import os
+import sys
+import math
 import FreeCAD
 from FreeCAD import Base
 import Spreadsheet
 
 class Plot(object):
-    def __init__(self, speed, resis, ship):
+    def __init__(self, speed, resis, CF, CR, CA, CT, ship):
         """ Constructor. performs the plot and shows it.
         @param Speed, Ship speed.
         @param Resis, Resistance computed.
         @param ship Active ship instance.
         """
         self.plot(speed, resis, ship)
-        self.spreadSheet(speed, resis, ship)
+        self.plotCoeff(speed, CF, CR, CT, ship)
+        self.spreadSheet(speed, resis, CF, CR, CA, CT, ship)
 
     def plot(self, speed, resis, ship):
         """ Perform the areas curve plot.
@@ -54,20 +57,83 @@ class Plot(object):
                 FreeCAD.Console.PrintWarning(msg + '\n')
                 return True
 
-        areas = Plot.plot( speed, resis, 'Resistance Amadeo method')
+        areas = Plot.plot(speed, resis, 'Resistance Amadeo method')
         areas.line.set_linestyle('-')
         areas.line.set_linewidth(2.0)
         areas.line.set_color((0.0, 0.0, 0.0))
-       
+
         # Write axes titles
-        Plot.xlabel(r'$x \; \mathrm{m/s}$')
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
         Plot.ylabel(r'$Resistance \; \mathrm{kN}$')
         # Show grid
         Plot.grid(True)
         # End
         return False
 
-    def spreadSheet(self, speed, resis, ship):
+    def plotCoeff(self, speed, CF, CR ,CT, ship):
+        """ Perform the areas curve plot.
+        @param speed, Ship speed.
+        @param CF, Ship speed.
+        @param CR, Resistance computed.
+        @param CT, Resistance computed.
+        @param ship Active ship instance.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        ax = Plot.axes()
+
+        plt = Plot.figure('Coefficients')
+        self.plt2 = plt
+        
+
+        CF = Plot.plot(speed, CF, r'$CF$')
+        CF.line.set_linestyle('-')
+        CF.line.set_linewidth(2.0)
+        CF.line.set_color((0.0, 0.0, 0.0))
+        self.CF = CF
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$CF$ (Friction coefficient)')
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        
+        CR = Plot.plot(speed, CR, r'$CR$')
+        CR.line.set_linestyle('-')
+        CR.line.set_linewidth(2.0)
+        CR.line.set_color((1.0, 0.0, 0.0))
+        self.CR = CR
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$CR$ (Residual resistance coefficient)')
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        
+        CT = Plot.plot(speed, CT, r'$CT$')
+        CT.line.set_linestyle('-')
+        CT.line.set_linewidth(2.0)
+        CT.line.set_color((0.0, 0.0, 1.0))
+        self.CT = CT
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$CT$ (Total resistance coefficient)')
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        
+        # Show grid
+        Plot.grid(True)
+        Plot.legend(True)
+        # End
+        return False
+
+    def spreadSheet(self, speed, resis, CF, CR, CA, CT, ship):
         """ Write the output data file.
         @param Speed, Ship speed.
         @param Resis, Resistance computed.
@@ -79,11 +145,21 @@ class Plot(object):
         # Print the header
         s.set("A1", "speed [m/s]")
         s.set("B1", "resistance [kN]")
+        s.set("C1", "CF")
+        s.set("D1", "CR")
+        s.set("E1", "CA")
+        s.set("F1", "CT")
+        
 
         # Print the data
         for i in range(len(resis)):
             s.set("A{}".format(i + 2), str(speed[i]))
             s.set("B{}".format(i + 2), str(resis[i]))
+            s.set("C{}".format(i + 2), str(CF[i]))
+            s.set("D{}".format(i + 2), str(CR[i]))
+            s.set("E{}".format(i + 2), str(CA))
+            s.set("F{}".format(i + 2), str(CT[i]))
+            
 
         # Recompute
         FreeCAD.activeDocument().recompute()
