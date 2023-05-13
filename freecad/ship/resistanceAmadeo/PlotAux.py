e20b7dfb3eb66e32e039aa424e137ed979632afd
Juan Manuel <juanma.munozgodin@gmail.com>
2023-05-13 14:14:24 +0200

    Including power graph, and axis modification

--- freecad/ship/resistanceAmadeo/PlotAux.py
+++ freecad/ship/resistanceAmadeo/PlotAux.py
@@ -22,13 +22,13 @@
 
 import os
 import sys
-import math
+import numpy as np
 import FreeCAD
 from FreeCAD import Base
 import Spreadsheet
 
 class Plot(object):
-    def __init__(self, speed, resis, CF, CR, CA, CT, ship):
+    def __init__(self, speed, resis, CF, CR, CA, CT, EKW, BKW, ship):
         """ Constructor. performs the plot and shows it.
         @param Speed, Ship speed.
         @param Resis, Resistance computed.
@@ -36,14 +36,17 @@ class Plot(object):
         @param CR, residual resistance coefficient.
         @param CA, Roughness coefficient
         @param CT, Total resistance coefficient.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
         @param ship Active ship instance.
         """
         self.plot(speed, resis, ship)
+        self.plotPower(speed, EKW, BKW, ship)
         self.plotCoeff(speed, CF, CR, CT, CA, ship)
-        self.spreadSheet(speed, resis, CF, CR, CA, CT, ship)
+        self.spreadSheet(speed, resis, CF, CR, CA, CT, EKW, BKW, ship)
 
     def plot(self, speed, resis, ship):
-        """ Perform the areas curve plot.
+        """ Perform Amadeo resistance pplot.
         @param Speed, Ship speed.
         @param Resis, Resistance computed.
         @param ship Active ship instance.
@@ -61,27 +64,86 @@ class Plot(object):
                 FreeCAD.Console.PrintWarning(msg + '\n')
                 return True
 
-        plt = Plot.figure('R - V')
+        plt = Plot.figure('Resistance - V')
         self.plt = plt
         
-        areas = Plot.plot(speed, resis, 'Resistance Amadeo method')
-        areas.line.set_linestyle('-')
-        areas.line.set_linewidth(2.0)
-        areas.line.set_color((0.0, 0.0, 0.0))
-
         # Write axes titles
+        ax = Plot.axes()
         Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
         Plot.ylabel(r'$Resistance \; \mathrm{kN}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Amadeo Resistance vs Speed', fontweight='bold')
+        
+        resis = Plot.plot(speed, resis, r'$Resistance \; \mathrm{kN}$')
+        resis.line.set_linestyle('-')
+        resis.line.set_linewidth(2.0)
+        resis.line.set_color((0.0, 0.0, 0.0))
+        
         # Show grid
         Plot.grid(True)
         # End
         return False
+    
+    def plotPower(self, speed, EKW, BKW, ship):
+        """ Perform Amadeo power plot.
+        @param Speed, Ship speed.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
+        @param ship Active ship instance.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        plt = Plot.figure('Power - V')
+        self.plt3 = plt
+        
+        # Write axes titles
+        ax = Plot.axes()
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$Power \; \mathrm{kW}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Amadeo Power vs Speed', fontweight='bold')
+        
+        EKW = Plot.plot(speed, EKW, r'$EKW, \; Efficient \; power$')
+        EKW.line.set_linestyle('-')
+        EKW.line.set_linewidth(2.0)
+        EKW.line.set_color((0.0, 0.0, 1.0))
+        self.EKW = EKW
+        
+        BKW = Plot.plot(speed, BKW, r'$BKW, \; Break \; power$')
+        BKW.line.set_linestyle('-')
+        BKW.line.set_linewidth(2.0)
+        BKW.line.set_color((1.0, 0.0, 0.0))
+        self.BKW = BKW
+        
+        
+        # Show grid
+        Plot.grid(True)
+        Plot.legend()
+        # End
+        return False
 
     def plotCoeff(self, speed, CF, CR ,CT, CA, ship):
-        """ Perform the areas curve plot.
+        """ Perform Amadeo coefficients plot.
         @param speed, Ship speed.
         @param CF, Frictional resistance coefficient.
-        @param CR, residual resistance coefficient.
+        @param CR, Residual resistance coefficient.
         @param CA, Roughness coefficient
         @param CT, Total resistance coefficient.
         @param ship Active ship instance.
@@ -103,68 +165,49 @@ class Plot(object):
         plt = Plot.figure('Coefficients')
         self.plt2 = plt
         
-        for i in range(0, 3):
-            ax = Plot.addNewAxes()
-            for loc, spine in ax.spines.items():
-                if loc in ['right', 'left']:
-                    spine.set_position(('outward', (i + 1) * 35))
-            Plot.grid(True)
-
-        axes = Plot.axesList()
-        for ax in axes:
-            ax.set_position([0.1, 0.35,  0.65,  0.8])
-        
-        plt.axes = axes[0]
-        CF = Plot.plot(speed, CF, r'$CF$')
-        CF.line.set_linestyle('-')
-        CF.line.set_linewidth(2.0)
-        CF.line.set_color((0.0, 0.0, 0.0))
-        self.CF = CF
+        ax = Plot.axes()
         Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CF$ (Friction coefficient)')
+        Plot.ylabel(r'$Resistance \; coefficients$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
         ax.xaxis.label.set_fontsize(15)
         ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Amadeo Resistance Coefficients vs Speed', fontweight='bold')
         
-        plt.axes = axes[1]
-        CR = Plot.plot(speed, CR, r'$CR$')
-        CR.line.set_linestyle('-')
-        CR.line.set_linewidth(2.0)
-        CR.line.set_color((1.0, 0.0, 0.0))
-        self.CR = CR
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CR$ (Residual resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
         
-        plt.axes = axes[2]
-        CT = Plot.plot(speed, CT, r'$CT$')
+        plt.axes.ticklabel_format(axis = 'y', scilimits = (0,0))
+        
+        CT = Plot.plot(speed, CT, r'$CT$, Total resistance coefficient')
         CT.line.set_linestyle('-')
         CT.line.set_linewidth(2.0)
         CT.line.set_color((0.0, 0.0, 1.0))
         self.CT = CT
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CT$ (Total resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
         
-        plt.axes = axes[3]
-        CA = Plot.plot(speed, CA, r'$CA$')
+        CA = Plot.plot(speed, CA, r'$CA$, Roughness coefficient')
         CA.line.set_linestyle('-')
         CA.line.set_linewidth(2.0)
         CA.line.set_color((0.2, 0.8, 0.2))
         self.CA = CA
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CT$ (Total resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
+        
+        CF = Plot.plot(speed, CF, r'$CF$, Frictional resistance coefficient')
+        CF.line.set_linestyle('-')
+        CF.line.set_linewidth(2.0)
+        CF.line.set_color((0.0, 0.0, 0.0))
+        self.CF = CF
+
+        CR = Plot.plot(speed, CR, r'$CR$, Residual resistance coefficient')
+        CR.line.set_linestyle('-')
+        CR.line.set_linewidth(2.0)
+        CR.line.set_color((1.0, 0.0, 0.0))
+        self.CR = CR
         
         # Show grid
         Plot.grid(True)
-        Plot.legend(True)
+        Plot.legend()
         # End
         return False
 
-    def spreadSheet(self, speed, resis, CF, CR, CA, CT, ship):
+    def spreadSheet(self, speed, resis, CF, CR, CA, CT, EKW, BKW, ship):
         """ Write the output data file.
         @param Speed, Ship speed.
         @param Resis, Resistance computed.
@@ -172,6 +215,8 @@ class Plot(object):
         @param CR, residual resistance coefficient.
         @param CA, Roughness coefficient
         @param CT, Total resistance coefficient.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
         @param ship Active ship instance.
         """
         s = FreeCAD.activeDocument().addObject('Spreadsheet::Sheet',
@@ -180,21 +225,24 @@ class Plot(object):
         # Print the header
         s.set("A1", "speed [m/s]")
         s.set("B1", "resistance [kN]")
-        s.set("C1", "CF")
-        s.set("D1", "CR")
-        s.set("E1", "CA")
-        s.set("F1", "CT")
+        s.set("C1", "CF * 10^3")
+        s.set("D1", "CR * 10^3")
+        s.set("E1", "CA * 10^3")
+        s.set("F1", "CT * 10^3")
+        s.set("G1", "EKW [kW]")
+        s.set("H1", "BKW [kW]")
         
 
         # Print the data
         for i in range(len(resis)):
             s.set("A{}".format(i + 2), str(speed[i]))
             s.set("B{}".format(i + 2), str(resis[i]))
-            s.set("C{}".format(i + 2), str(CF[i]))
-            s.set("D{}".format(i + 2), str(CR[i]))
-            s.set("E{}".format(i + 2), str(CA[i]))
-            s.set("F{}".format(i + 2), str(CT[i]))
-            
+            s.set("C{}".format(i + 2), str(CF[i] * 1000))
+            s.set("D{}".format(i + 2), str(CR[i] * 1000))
+            s.set("E{}".format(i + 2), str(CA[i] * 1000))
+            s.set("F{}".format(i + 2), str(CT[i] * 1000))
+            s.set("G{}".format(i + 2), str(EKW[i]))
+            s.set("H{}".format(i + 2), str(BKW[i]))
 
         # Recompute
         FreeCAD.activeDocument().recompute()
