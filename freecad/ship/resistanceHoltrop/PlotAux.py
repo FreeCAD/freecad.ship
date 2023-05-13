9c4cb9809a0c818f1950a0d819a87482276f40b3
Juan Manuel <juanma.munozgodin@gmail.com>
2023-05-13 14:15:04 +0200

    Including power graph, and axis modifications

--- freecad/ship/resistanceHoltrop/PlotAux.py
+++ freecad/ship/resistanceHoltrop/PlotAux.py
@@ -29,7 +29,7 @@ import Spreadsheet
 
 
 class Plot(object):
-    def __init__(self, speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, ship):
+    def __init__(self, speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, EKW, BKW, ship):
         """ Constructor. performs the plot and shows it.
         @param Speed, Ship speed.
         @param Rtotal, Resistance computed.
@@ -42,14 +42,17 @@ class Plot(object):
         @param CTR, Additional resistance coefficient due to the inmersed.
                                                                     transom.
         @param CA, Model-ship correlation resistance coefficient.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
         @param ship Active ship instance.
         """
         self.plot(speed, Rtotal, ship)
+        self.plotPower(speed, EKW, BKW, ship)
         self.plotCoeff(speed, CT, CF, CAPP, Cw, CB, CTR, CA, ship)
-        self.spreadSheet(speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, ship)
+        self.spreadSheet(speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, EKW, BKW, ship)
 
     def plot(self, speed, Rtotal, ship):
-        """ Perform the areas curve plot.
+        """ Perform HOltrop resistance plot.
         @param Speed, Ship speed.
         @param Rtotal, Resistance computed.
         @param ship Active ship instance.
@@ -67,24 +70,83 @@ class Plot(object):
                 FreeCAD.Console.PrintWarning(msg + '\n')
                 return True
             
-        plt = Plot.figure('R - V')
+        plt = Plot.figure('Resistance - V')
         self.plt = plt
+        
+        ax = Plot.axes()
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$Resistance \; \mathrm{kN}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Holtrop Resistance vs Speed', fontweight='bold')
         
         areas = Plot.plot(speed, Rtotal, 'Resistance Holtrop method')
         areas.line.set_linestyle('-')
         areas.line.set_linewidth(2.0)
         areas.line.set_color((0.0, 0.0, 0.0))
 
-        # Write axes titles
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$Resistance \; \mathrm{kN}$')
         # Show grid
         Plot.grid(True)
         # End
         return False
 
+    def plotPower(self, speed, EKW, BKW, ship):
+        """ Perform Holtrop power plot.
+        @param Speed, Ship speed.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
+        @param ship Active ship instance.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+    
+        plt = Plot.figure('Power - V')
+        self.plt3 = plt
+        
+        # Write axes titles
+        ax = Plot.axes()
+        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
+        Plot.ylabel(r'$Power \; \mathrm{kW}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Holtrop Power vs Speed', fontweight='bold')
+        
+        EKW = Plot.plot(speed, EKW, r'$EKW, \; Efficient \; power$')
+        EKW.line.set_linestyle('-')
+        EKW.line.set_linewidth(2.0)
+        EKW.line.set_color((0.0, 0.0, 1.0))
+        self.EKW = EKW
+        
+        BKW = Plot.plot(speed, BKW, r'$BKW, \; Break \; power$')
+        BKW.line.set_linestyle('-')
+        BKW.line.set_linewidth(2.0)
+        BKW.line.set_color((1.0, 0.0, 0.0))
+        self.BKW = BKW
+        
+        
+        # Show grid
+        Plot.grid(True)
+        Plot.legend()
+        # End
+        return False
+    
+    
     def plotCoeff(self, speed, CT, CF, CAPP, Cw, CB, CTR, CA, ship):
-        """ Perform the areas curve plot.
+        """ Perform Holtrop coefficients plot.
         @param speed, Ship speed.
         @param CT, Total resistance coefficient
         @param CF, Frictional resistance coefficient.
@@ -109,80 +171,62 @@ class Plot(object):
                 FreeCAD.Console.PrintWarning(msg + '\n')
                 return True
 
-        ax = Plot.axes()
-
-        plt = Plot.figure('Coefficients - V')
+        plt = Plot.figure('Coefficients')
         self.plt2 = plt
         
-        CT = Plot.plot(speed, CT, r'$CT$')
-        CT.line.set_linestyle('-')
-        CT.line.set_linewidth(2.0)
-        CT.line.set_color((0.0, 0.0, 0.0))
-        self.CT = CT
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CT$ (Total resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
+        plt.axes.ticklabel_format(axis = 'y', scilimits = (0,0))
         
-        CF = Plot.plot(speed, CF, r'$CF$')
-        CF.line.set_linestyle('-')
-        CF.line.set_linewidth(2.0)
-        CF.line.set_color((0.0, 1.0, 0.0))
-        self.CF = CF
+        ax = Plot.axes()
         Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CF$ (Friction coefficient)')
+        Plot.ylabel(r'$Resistance \; coefficients$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
         ax.xaxis.label.set_fontsize(15)
         ax.yaxis.label.set_fontsize(15)
-        
-        CAPP = Plot.plot(speed, CAPP, r'$CAPP$')
-        CAPP.line.set_linestyle('-')
-        CAPP.line.set_linewidth(2.0)
-        CAPP.line.set_color((1.0, 0.0, 0.0))
-        self.CAPP = CAPP
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CAPP$ (Appendage resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
-        
-        Cw = Plot.plot(speed, Cw, r'$Cw$')
-        Cw.line.set_linestyle('-')
-        Cw.line.set_linewidth(2.0)
-        Cw.line.set_color((0.0, 0.0, 1.0))
-        self.Cw = Cw
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$Cw$ (Wave resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
-        
-        CB = Plot.plot(speed, CB, r'$CB$')
-        CB.line.set_linestyle('-')
-        CB.line.set_linewidth(2.0)
-        CB.line.set_color((0.8, 0.2, 0.8))
-        self.CB = CB
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CB$ (Bulbous additional resistance coefficient )')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
-        
-        CTR = Plot.plot(speed, CTR, r'$CTR$')
-        CTR.line.set_linestyle('-')
-        CTR.line.set_linewidth(2.0)
-        CTR.line.set_color((0.5, 0.5, 0.5))
-        self.CTR = CTR
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CTR$ (Imnserd transom additional resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Holtrop Resistance Coefficients vs Speed', fontweight='bold')
         
         CA = Plot.plot(speed, CA, r'$CA$')
         CA.line.set_linestyle('-')
         CA.line.set_linewidth(2.0)
         CA.line.set_color((0.2, 0.8, 0.2))
         self.CA = CA
-        Plot.xlabel(r'$Speed \; \mathrm{m/s}$')
-        Plot.ylabel(r'$CT$ (Total resistance coefficient)')
-        ax.xaxis.label.set_fontsize(15)
-        ax.yaxis.label.set_fontsize(15)
+        
+        CF = Plot.plot(speed, CF, r'$CF$')
+        CF.line.set_linestyle('-')
+        CF.line.set_linewidth(2.0)
+        CF.line.set_color((0.0, 0.0, 0.0))
+        self.CF = CF
+        
+        Cw = Plot.plot(speed, Cw, r'$Cw$')
+        Cw.line.set_linestyle('-')
+        Cw.line.set_linewidth(2.0)
+        Cw.line.set_color((1.0, 0.0, 0.0))
+        self.Cw = Cw
+
+        CAPP = Plot.plot(speed, CAPP, r'$CAPP$')
+        CAPP.line.set_linestyle('-')
+        CAPP.line.set_linewidth(2.0)
+        CAPP.line.set_color((1.0, 1.0, 0.0))
+        self.CAPP = CAPP
+        
+        CB = Plot.plot(speed, CB, r'$CB$')
+        CB.line.set_linestyle('-')
+        CB.line.set_linewidth(2.0)
+        CB.line.set_color((0.8, 0.2, 0.8))
+        
+
+        CTR = Plot.plot(speed, CTR, r'$CTR$')
+        CTR.line.set_linestyle('-')
+        CTR.line.set_linewidth(2.0)
+        CTR.line.set_color((0.5, 0.5, 0.5))
+        self.CTR = CTR
+
+        CT = Plot.plot(speed, CT, r'$CT$')
+        CT.line.set_linestyle('-')
+        CT.line.set_linewidth(2.0)
+        CT.line.set_color((0.0, 0.0, 1.0))
+        self.CT = CT
+
         
         # Show grid
         Plot.grid(True)
@@ -190,7 +234,7 @@ class Plot(object):
         # End
         return False
 
-    def spreadSheet(self, speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, ship):
+    def spreadSheet(self, speed, Rtotal, CT, CF, CAPP, Cw, CB, CTR, CA, EKW, BKW, ship):
         """ Write the output data file.
         @param speed, Ship speed.
         @param CT, Total resistance coefficient
@@ -202,6 +246,8 @@ class Plot(object):
         @param CTR, Additional resistance coefficient due to the inmersed.
                                                                     transom.
         @param CA, Model-ship correlation resistance coefficient.
+        @param EKW, Efficient power.
+        @param BKW, Break power.
         @param ship Active ship instance.
         """
         s = FreeCAD.activeDocument().addObject('Spreadsheet::Sheet',
@@ -210,26 +256,30 @@ class Plot(object):
         # Print the header
         s.set("A1", "speed [m/s]")
         s.set("B1", "resistance [kN]")
-        s.set("C1", "CT")
-        s.set("D1", "CF")
+        s.set("C1", "CT * 10^3")
+        s.set("D1", "CF * 10^3")
         s.set("E1", "CAPP")
-        s.set("F1", "Cw")
-        s.set("G1", "CB")
-        s.set("H1", "CTR")
-        s.set("I1", "CA")
+        s.set("F1", "Cw * 10^4")
+        s.set("G1", "CB * 10^3")
+        s.set("H1", "CTR * 10^3")
+        s.set("I1", "CA * 10^3")
+        s.set("J1", "BKW [kW]")
+        s.set("K1", "EKW [kW]")
         
 
         # Print the data
         for i in range(len(Rtotal)):
             s.set("A{}".format(i + 2), str(speed[i]))
             s.set("B{}".format(i + 2), str(Rtotal[i]))
-            s.set("C{}".format(i + 2), str(CT[i]))
-            s.set("D{}".format(i + 2), str(CF[i]))
-            s.set("E{}".format(i + 2), str(CAPP[i]))
-            s.set("F{}".format(i + 2), str(Cw[i]))
-            s.set("G{}".format(i + 2), str(CB[i]))
-            s.set("H{}".format(i + 2), str(CTR[i]))
-            s.set("I{}".format(i + 2), str(CA[i]))
+            s.set("C{}".format(i + 2), str(CT[i] * 1000))
+            s.set("D{}".format(i + 2), str(CF[i] * 1000))
+            s.set("E{}".format(i + 2), str(CAPP[i] * 1000))
+            s.set("F{}".format(i + 2), str(Cw[i] * 10000))
+            s.set("G{}".format(i + 2), str(CB[i] * 1000))
+            s.set("H{}".format(i + 2), str(CTR[i]  * 1000))
+            s.set("I{}".format(i + 2), str(CA[i]  * 1000))
+            s.set("J{}".format(i + 2), str(EKW[i]))
+            s.set("K{}".format(i + 2), str(BKW[i]))
             
 
         # Recompute
