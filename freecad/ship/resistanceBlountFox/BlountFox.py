ac0d0d7b55da80594d3729894ce638822f57a00a
danis <danisbarrena@gmail.com>
2025-02-19 06:46:46 +0100

    Blount and Fox method creation

---
+++ freecad/ship/resistanceBlountFox/BlountFox.py
@@ -0,0 +1,285 @@
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2011, 2016 Jose Luis Cercos Pita <jlcercos@gmail.com>   *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+import numpy as np
+from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+import warnings
+
+# Avoid printing warnings type RuntimeWarning
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+# Constants
+rho = 1025  # Salt water density [kg/m3]
+g = 9.81  # Gravity acceleration in Earth [m/s2]
+nu = 1.1892 * 10 ** -6  # Salt water dynamic viscosity [m2/s]
+kn_to_ms = 0.5144  # Conversion factor from knots to meters per second
+
+
+def calculate_n(disp, trim_angle):
+    return disp * g * np.cos(trim_angle)
+
+
+def calculate_cv(v, g, b):
+    return v/np.sqrt(g * b)
+
+
+def calculate_fv(v, g, disp):
+    return v/np.sqrt(g*np.cbrt(disp/rho))
+
+
+def calculate_cl_b(v, b, disp):
+    return disp * g / (0.5 * rho * v**2 * b**2)
+
+
+def equation_01(cl_0, deadrise_angle, cl_b):
+    result = cl_b - cl_0 + 0.0065 * deadrise_angle * cl_0**0.6
+    return result
+
+
+def equation_02(lb_lambda, trim_angle, cv, cl_0):
+    result = cl_0 - (np.degrees(trim_angle)**1.1) * (0.012 * lb_lambda**0.5 + 0.0055 * lb_lambda**2.5 / cv**2)
+    return result
+
+
+def calculate_cl_0d(lb_lambda, trim_angle):
+    return 0.0120 * lb_lambda**0.5 * trim_angle**1.1
+
+
+def calculate_cl_bd(cl_0d, deadrise_angle):
+    return cl_0d - cl_0d**0.6 * deadrise_angle * 0.0065
+
+
+def calculate_disp_d(rho, v, b, cl_bd):
+    return 0.5 * rho * v**2 * b**2 * cl_bd / g
+
+
+def calculate_pd(disp_d, lb_lambda, b, trim_angle):
+    return disp_d * g / (lb_lambda * b**2 * np.cos(trim_angle))
+
+
+def calculate_v_1(v, pd, rho):
+    return v * (1 - 2 * pd / (rho * v**2))**0.5
+
+
+def calculate_re_lambda(v_1, lb_lambda, b, nu):
+    return v_1 * lb_lambda * b / nu
+
+
+def calculate_cf_schoenherr(cf, re_lambda):
+    result = 0.242 / np.sqrt(cf) - np.log10(cf * re_lambda)
+    return result
+
+
+def calculate_dfriction(cf, rho, v_1, lb_lambda, b, deadrise_angle):
+    return cf * rho * v_1**2 * lb_lambda * b**2 * 0.5 / np.cos(np.radians(deadrise_angle))
+
+
+def calculate_cpressure(dpressure, rho, v_1, lb_lambda, b):
+    return dpressure / (rho * v_1**2 * lb_lambda * b**2 * 0.5)
+
+
+def calculate_ctotal(drag, rho, v_1, lb_lambda, b):
+    return drag / (rho * v_1**2 * lb_lambda * b**2 * 0.5)
+
+
+def calculate_cp(cv, lb_lambda):
+    return 0.75 - 1 / (5.21 * cv**2 / lb_lambda**2 + 2.39)
+
+
+def equation_03(disp, lcg, n, cp, lb_lambda, b):
+    return disp * g * lcg - n * cp * lb_lambda * b
+
+
+def calculate_drag(disp, trim_angle, dfric):
+    return disp * g * np.tan(trim_angle) + dfric / np.cos(trim_angle)
+
+
+def calculate_m(lcg, b, fv):
+    return 0.98 + 2 * (lcg / b)**1.45 * np.e**(-2 * (fv - 0.85)) - 3 * (lcg / b) * np.e**(-3 * (fv - 0.85))
+
+
+def blount_and_fox(b, vmin, vmax, nspeeds, disp, deadrise_angle, lcg, trim_step, Sea_Margin, efficiency):
+    tol = trim_step / 2.5
+    trim_step = np.radians(trim_step)
+
+    assert b > 0
+    assert vmin > 0
+    assert vmax > vmin
+    assert nspeeds > 1
+    assert disp > 0
+    assert deadrise_angle > 0
+    assert lcg > 0
+
+    if trim_step < 0:
+        trim_step = np.radians(0.01)
+
+    # Units conversions
+    vmin = vmin * kn_to_ms
+    vmax = vmax * kn_to_ms
+    disp = disp * 1e3
+
+    speeds = np.linspace(vmin, vmax, nspeeds)
+    drag = np.zeros(nspeeds, dtype=float)
+    drag_savitsky = np.zeros(nspeeds, dtype=float)
+    trims = np.zeros(nspeeds, dtype=float)
+    lift = np.zeros(nspeeds, dtype=float)
+    dfriction = np.zeros(nspeeds, dtype=float)
+    dpressure = np.zeros(nspeeds, dtype=float)
+    lam = np.zeros(nspeeds, dtype=float)
+    reynolds = np.zeros(nspeeds, dtype=float)
+    cfric = np.zeros(nspeeds, dtype=float)
+    cpres = np.zeros(nspeeds, dtype=float)
+    ctotal = np.zeros(nspeeds, dtype=float)
+    speed = np.zeros(nspeeds, dtype=float)
+    speed_savitsky = np.zeros(nspeeds, dtype=float)
+
+    # Initialize variables to zero
+    dfric, trim_angle, cl_b, cl_0, n, lb_lambda, lp, re_lambda, trim, cf \
+        = None, None, None, None, None, None, None, None, None, None
+
+    j, k = 0, 0
+    out_of_limits = False
+    rised = False
+    trim_minimum = 1.0
+
+    for i in range(nspeeds):
+        v = speeds[i]
+
+        trim_min = np.radians(trim_minimum)
+        trim_max = np.radians(15.0)
+        trim_angles = np.arange(trim_min, trim_max, trim_step)
+
+        fv = calculate_fv(v, g, disp)
+
+        for trim_angle in trim_angles:
+            trim = np.rad2deg(trim_angle)  # Just convert the trim into degrees to plot it later
+            n = calculate_n(disp, trim_angle)
+            cv = calculate_cv(v, g, b)
+            cl_b = calculate_cl_b(v, b, disp)
+
+            cl_0 = fsolve(equation_01, np.array([0.1]), args=(deadrise_angle, cl_b))
+            cl_0 = cl_0[0]
+            lb_lambda = fsolve(equation_02, np.array([3]), args=(trim_angle, cv, cl_0))
+            lb_lambda = lb_lambda[0]
+
+            cl_0d = calculate_cl_0d(lb_lambda, trim_angle)
+            cl_bd = calculate_cl_bd(cl_0d, deadrise_angle)
+            disp_d = calculate_disp_d(rho, v, b, cl_bd)
+            pd = calculate_pd(disp_d, lb_lambda, b, trim_angle)
+            v_1 = calculate_v_1(v, pd, rho) * 0.99
+            re_lambda = calculate_re_lambda(v_1, lb_lambda, b, nu)
+            cf = fsolve(calculate_cf_schoenherr, np.array([0.001]), args=re_lambda)
+            cf = cf[0] + 0.0004
+            dfric = calculate_dfriction(cf, rho, v_1, lb_lambda, b, deadrise_angle)
+            cp = calculate_cp(cv, lb_lambda)
+
+            B = disp * g * lcg
+            equilibrium = equation_03(disp, lcg, n, cp, lb_lambda, b)
+
+            if np.rad2deg(trim_angle) > 14.980:
+                out_of_limits = True
+                print('WARNING: Could not get a solution for the maximum velocity introduced, it is out of limits!')
+                break
+
+            # Stop the iteration if the equilibrium is less than the set tolerance
+            if abs(equilibrium) < tol * B:
+                break
+
+        if out_of_limits:
+            break
+
+        if np.rad2deg(trim) > 2.0:
+            trim_minimum = 2.0
+
+        if fv >= 1:
+            m = calculate_m(lcg, b, fv)
+
+            drag[j] = calculate_drag(disp, trim_angle, dfric) * m
+            dfriction[j] = dfric
+            trims[j] = trim
+            lift[j] = n
+            dpressure[j] = disp * g * np.tan(trim_angle)
+            lam[j] = lb_lambda
+            reynolds[j] = re_lambda / 1000
+            cfric[j] = cf * m
+            cpres[j] = calculate_cpressure(dpressure[j], rho, v_1, lb_lambda, b) * m
+            ctotal[j] = calculate_ctotal(drag[j], rho, v_1, lb_lambda, b)
+            speed[j] = v
+
+            if cv >= 1:
+                drag_savitsky[k] = calculate_drag(disp, trim_angle, dfric)
+                speed_savitsky[k] = v
+                k += 1
+
+            j += 1
+
+    # Trim vectors, deleting zeros
+    drag = drag[:j]
+    speed = speed[:j]
+    trims = trims[:j]
+    lift = lift[:j]
+    dfriction = dfriction[:j]
+    dpressure = dpressure[:j]
+    lam = lam[:j]
+    reynolds = reynolds[:j]
+    cfric = cfric[:j]
+    cpres = cpres[:j]
+    ctotal = ctotal[:j]
+    drag_savitsky = drag_savitsky[:k]
+    speed_savitsky = speed_savitsky[:k]
+
+    EKW = drag * speed * (1 + Sea_Margin)
+    BKW = EKW / efficiency
+
+    return (drag, speed/kn_to_ms, trims, lift, dfriction, dpressure, lam, reynolds, cfric, cpres, ctotal, EKW, BKW,
+            drag_savitsky, speed_savitsky/kn_to_ms)
+
+
+if __name__ == '__main__':
+
+    # Example to test the code
+    Vmin = 10                           # Min velocity [m/s]
+    Vmax = 60                           # Max velocity [m/s]
+    Nspeeds = 100                       # Number of speeds [-]
+    B = 6.5                             # Beam [m]
+    disp = 70                           # Displacement [kg]
+    LCG = 10                            # Longitudinal center of gravity [m]
+    Deadrise_angle = 20                 # Deadrise angle []
+
+    trim_step = 0.01
+    Sea_Margin = 0.15
+    efficiency = 0.6
+
+    (drag, speeds, trims, lift, dfriction, dpressure, lam, rein, cfri, cpres, ctotal, EKW, BKW, drag_savitsky,
+     speed_savitsky) = (
+        blount_and_fox(B, Vmin, Vmax, Nspeeds, disp, Deadrise_angle, LCG, trim_step, Sea_Margin, efficiency))
+
+    plt.plot(speeds, drag, label="Resistance", color="red")
+    plt.xlim(left=0)
+    plt.ylim(bottom=0)
+    plt.xlabel("Speeds [kn]")
+    plt.ylabel("Total resistance [kN]")
+    plt.title("Savitsky Resistance")
+    plt.legend()
+    plt.grid()
+    plt.show()
---
+++ freecad/ship/resistanceBlountFox/PlotAux.py
@@ -0,0 +1,327 @@
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2011, 2016 Jose Luis Cercos Pita <jlcercos@gmail.com>   *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
+import os
+import sys
+import math
+import FreeCAD
+from FreeCAD import Base
+import Spreadsheet
+
+
+class Plot(object):
+    def __init__(self, speeds, trims, drag, dfriction, dpressure, cfri, cpres, ctotal, EKW, BKW, drag_savitsky,
+                 savitsky_check, speed_savitsky):
+        """ Constructor. performs the plot and shows it.
+        @param speeds, Ship speed.
+        @param trims, Equilibrium trim computed.
+        @param drag, Resistance computed.
+        @param dfriction, Friction Resistance computed.
+        @param dpressure, Pressure Resistance computed.
+        @param cfri, Frictional resistance coefficient.
+        @param cpres, Pressure coefficient.
+        @param ctotal, Total resistance coefficient.
+        @param EKW, Effective power.
+        @param BKW, Break power.
+        @param drag_savitsky, Savitsky Resistance computed.
+        @param speed_savitsky, Savitsky Resistance speeds.
+        @param savitsky_check, boolean key to plot Savitsky Resistance.
+        """
+        self.plot(speeds, drag, dfriction, dpressure, drag_savitsky, speed_savitsky, savitsky_check)
+        self.plotTrim(speeds, trims)
+        self.plotPower(speeds, EKW, BKW)
+        self.plotCoeff(speeds, cfri, cpres, ctotal)
+        self.spreadSheet(speeds, trims, drag, dfriction, dpressure, cfri, cpres, ctotal, EKW, BKW)
+
+    def plot(self, speeds, drag, dfriction, dpressure, drag_savitsky, speed_savitsky, savitsky_check):
+        """ Perform Blount and Fox resistance plot.
+        @param speeds, Ship speeds.
+        @param drag, Resistance computed.
+        @param dfriction, Friction Resistance computed.
+        @param dpressure, Pressure Resistance computed.
+        @param drag_savitsky, Savitsky Resistance computed.
+        @param speed_savitsky, Savitsky Resistance speeds.
+        @param savitsky_check, boolean key to plot Savitsky Resistance.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        plt = Plot.figure('Resistance')
+        self.plt = plt
+
+        ax = Plot.axes()
+        ax.set_position([0.1, 0.1, 0.65, 0.8])
+        Plot.xlabel(r'$Speed \; \mathrm{[kn]}$')
+        Plot.ylabel(r'$Resistance \; \mathrm{[kN]}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Blount and Fox: Resistance vs Speed', fontweight='bold')
+
+        rtotal = Plot.plot(speeds, drag / 1000, r'$Resistance \; Blount \; and \; Fox$')
+        rtotal.line.set_linestyle('-')
+        rtotal.line.set_linewidth(2.0)
+        rtotal.line.set_color((0.0, 0.0, 0.0))
+        self.rtotal = rtotal
+
+        rfri = Plot.plot(speeds, dfriction / 1000, r'$Frictional \; resistance \; component$')
+        rfri.line.set_linestyle('-')
+        rfri.line.set_linewidth(2.0)
+        rfri.line.set_color((0.0, 0.0, 1.0))
+        self.rfri = rfri
+
+        rpress = Plot.plot(speeds, dpressure / 1000, r'$Pressure \; resistance \; component$')
+        rpress.line.set_linestyle('-')
+        rpress.line.set_linewidth(2.0)
+        rpress.line.set_color((1.0, 0.0, 0.0))
+        self.rpress = rpress
+
+        if savitsky_check:
+            rsav = Plot.plot(speed_savitsky, drag_savitsky / 1000, r'$Resistance \; Savitsky$')
+            rsav.line.set_linestyle('-')
+            rsav.line.set_linewidth(2.0)
+            rsav.line.set_color((0.0, 1.0, 0.0))
+            self.rsav = rsav
+
+        # Set axis limits
+        ax.set_xlim(0, max(speeds) * 1.1)
+        ax.set_ylim(0, max(drag/1000) * 1.1)
+        # Show grid
+        Plot.grid(True)
+        # Show legend
+        Plot.legend(True, pos=(1.05, 0.6))
+        # End
+        return False
+
+    def plotTrim(self, speeds, trims):
+        """ Perform Blount and Fox trim plot.
+        @param speeds, Ship speeds.
+        @param trims, Equilibrium trim computed.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        plt = Plot.figure('Trim')
+        self.plt = plt
+
+        ax = Plot.axes()
+        ax.set_position([0.1, 0.1, 0.65, 0.8])
+        Plot.xlabel(r'$Speeds \; \mathrm{[kn]}$')
+        Plot.ylabel(r'$Trim \; \mathrm{[deg]}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Blount and Fox: Trim angle vs Speed', fontweight='bold')
+
+        trim = Plot.plot(speeds, trims, 'Trim angle')
+        trim.line.set_linestyle('-')
+        trim.line.set_linewidth(2.0)
+        trim.line.set_color((0.0, 0.0, 0.0))
+        self.trim = trim
+
+        # Set axis limits
+        ax.set_xlim(0, max(speeds) * 1.1)
+        ax.set_ylim(0, max(trims) * 1.1)
+        # Show grid
+        Plot.grid(True)
+        # End
+        return False
+
+    def plotPower(self, speeds, _EKW, _BKW):
+        """ Perform Blount and Fox power plot.
+        @param speeds, Ship speeds.
+        @param _EKW, Effective power.
+        @param _BKW, Break power.
+        @return True if error happens.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        plt = Plot.figure('Power')
+        self.plt3 = plt
+
+        # Write axes titles
+        ax = Plot.axes()
+        ax.set_position([0.1, 0.1, 0.65, 0.8])
+        Plot.xlabel(r'$Speed \; \mathrm{[kn]}$')
+        Plot.ylabel(r'$Power \; \mathrm{[kW]}$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Blount and Fox: Power vs Speed', fontweight='bold')
+
+        EKW = Plot.plot(speeds, _EKW / 1000, r'$EKW, \; Effective \; power$')
+        EKW.line.set_linestyle('-')
+        EKW.line.set_linewidth(2.0)
+        EKW.line.set_color((0.0, 0.0, 1.0))
+        self.EKW = EKW
+
+        BKW = Plot.plot(speeds, _BKW / 1000, r'$BKW, \; Break \; power$')
+        BKW.line.set_linestyle('-')
+        BKW.line.set_linewidth(2.0)
+        BKW.line.set_color((1.0, 0.0, 0.0))
+        self.BKW = BKW
+
+        # Set axis limits
+        ax.set_xlim(0, max(speeds) * 1.1)
+        ax.set_ylim(0, max(_BKW / 1000) * 1.1)
+        # Show grid
+        Plot.grid(True)
+        # Show legend
+        Plot.legend(True, pos=(1.05, 0.6))
+        # End
+        return False
+
+    def plotCoeff(self, speeds, cfric, cpres, ctotal):
+        """ Perform Blount and Fox coefficients plot.
+        @param speeds, Ship speeds.
+        @param cfric, Frictional resistance coefficient.
+        @param cpres, Pressure coefficient.
+        @param ctotal, Total resistance coefficient.
+        """
+        try:
+            from FreeCAD.Plot import Plot
+        except ImportError:
+            try:
+                from freecad.plot import Plot
+            except ImportError:
+                msg = FreeCAD.Qt.translate(
+                    "ship_console",
+                    "Plot module is disabled")
+                FreeCAD.Console.PrintWarning(msg + '\n')
+                return True
+
+        plt = Plot.figure('Coefficient')
+        self.plt2 = plt
+
+        plt.axes.ticklabel_format(axis='y', scilimits=(0, 0))
+
+        ax = Plot.axes()
+        ax.set_position([0.05, 0.1, 0.62, 0.8])
+        Plot.xlabel(r'$Speed \; \mathrm{[kn]}$')
+        Plot.ylabel(r'$Resistance \; coefficient$')
+        ax.spines['right'].set_color((0.0, 0.0, 0.0))
+        ax.spines['top'].set_color((0.0, 0.0, 0.0))
+        ax.xaxis.label.set_fontsize(15)
+        ax.yaxis.label.set_fontsize(15)
+        ax.set_title('Blount and Fox: Resistance Coefficients vs Speed', fontweight='bold')
+
+        cfric = Plot.plot(speeds, cfric, r'$CF$, Schoenherr frictional coefficient')
+        cfric.line.set_linestyle('-')
+        cfric.line.set_linewidth(2.0)
+        cfric.line.set_color((0.0, 0.0, 1.0))
+        self.cfri = cfric
+
+        cpres = Plot.plot(speeds, cpres, r'$CP$, Pressure coefficient')
+        cpres.line.set_linestyle('-')
+        cpres.line.set_linewidth(2.0)
+        cpres.line.set_color((1.0, 0.0, 0.0))
+        self.cpres = cpres
+
+        ctotal = Plot.plot(speeds, ctotal, r'$CT$, Total resistance coefficient')
+        ctotal.line.set_linestyle('-')
+        ctotal.line.set_linewidth(2.0)
+        ctotal.line.set_color((0.0, 0.0, 0.0))
+        self.ctotal = ctotal
+
+        # Show grid
+        Plot.grid(True)
+        # Show legend
+        Plot.legend(True, pos=(1.35, 0.6))
+        # End
+        return False
+
+    def spreadSheet(self, speeds, trims, drag, dfriction, dpressure, cfri, cpres, ctotal, EKW, BKW):
+        """ Write the output data file.
+        @param speeds, Ship speeds.
+        @param trims, Equilibrium trim computed.
+        @param drag, Total resistance.
+        @param dfriction, Frictional resistance.
+        @param dpressure, Pressure resistance.
+        @param cfri, Frictional resistance coefficient.
+        @param cpres, Pressure coefficient.
+        @param ctotal, Total resistance coefficient.
+        @param EKW, Effective power.
+        @param BKW, Break power.
+        """
+
+        s = FreeCAD.activeDocument().addObject('Spreadsheet::Sheet', 'Resistance Blount and Fox method')
+
+        # Print the header
+        s.set("A1", "Speed [kn]")
+        s.set("B1", "Trim [deg]")
+        s.set("C1", "Total resistance [kN]")
+        s.set("D1", "Friction resistance [kN]")
+        s.set("E1", "Pressure resistance [kN]")
+        s.set("F1", "CF * 10^3")
+        s.set("G1", "CP * 10^3")
+        s.set("H1", "CT * 10^3")
+        s.set("I1", "EKW [kW]")
+        s.set("J1", "BKW [kW]")
+
+        # Print the data
+        for i in range(len(drag)):
+            s.set("A{}".format(i + 2), str(speeds[i]))
+            s.set("B{}".format(i + 2), str(trims[i]))
+            s.set("C{}".format(i + 2), str(drag[i] / 1000))
+            s.set("D{}".format(i + 2), str(dfriction[i] / 1000))
+            s.set("E{}".format(i + 2), str(dpressure[i] / 1000))
+            s.set("F{}".format(i + 2), str(cfri[i] * 1000))
+            s.set("G{}".format(i + 2), str(cpres[i] * 1000))
+            s.set("H{}".format(i + 2), str(ctotal[i] * 1000))
+            s.set("I{}".format(i + 2), str(EKW[i] / 1000))
+            s.set("J{}".format(i + 2), str(BKW[i] / 1000))
+
+        # Recompute
+        FreeCAD.activeDocument().recompute()
